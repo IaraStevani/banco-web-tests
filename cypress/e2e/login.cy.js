cf6578ad40c4d0f01a9ee0eeb50f1e8cc0628478
IaraStevani <iarastevanialves@gmail.com>
2025-10-02 08:16:32 -0300

    Adicionei o beforeEach para o acesso a página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,10 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  beforeEach(() => {
     // Arrange - preparação para o teste
     cy.visit('http://localhost:4000')
+  })
 
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act - ação do teste
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
@@ -11,17 +13,15 @@ describe('Login', () => {
     // Assert - verificação do teste
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
+  
   // .only roda apenas esse teste
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    // Arrange - preparação para o teste
-    cy.visit('http://localhost:4000') 
-
     // Act - ação do teste
     cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('54321') 
-    cy.contains('button','Entrar').click()
+    cy.get('#senha').click().type('54321')
+    cy.contains('button', 'Entrar').click()
 
     // Assert - verificação do teste
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
-  }) 
+  })
 }) 
